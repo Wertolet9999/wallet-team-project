453651d84e281c5512dce5cabc04f9dfa20cbe04
sydorenkorv <99117460+sydorenkorv@users.noreply.github.com>
2023-02-16 13:09:40 +0000

    regForm + LoginForm

---
+++ src/components/LoginForm/Form.styled.js
@@ -0,0 +1,269 @@
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import loginBgImgTab from '../../img/background/loginBgImgTab';
+import loginBgImgTabLarge from '../../img/background/loginBgImgTab@2x.png';
+import loginBgImgDesk from '../../img/background/loginBgImgDesk.png';
+import loginBgImgDeskLarge from '../../img/background/loginBgImgDesk@2x.png';
+import registerBgImgTab from '../../img/background/registerBgImgTab.png';
+import registerBgImgTabLarge from '../../img/background/registerBgImgTab@2x.png';
+import registerBgImgDesk from '../../img/background/registerBgImgDesk.png';
+import registerBgImgDeskLarge from '../../img/background/registerBgImgDesk@2x.png';
+
+export const Page = styled.div`
+  @media screen and (max-width: 767px) {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    height: 100vh;
+    background-color: ${p => p.theme.colors.white};
+  }
+`;
+
+export const CustomizedLoginPage = styled(Page)`
+  @media screen and (min-width: 768px) {
+    padding-top: 360px;
+    padding-bottom: 196px;
+    ::before {
+      content: '';
+      position: absolute;
+      top: 60px;
+      left: 114px;
+      z-index: -20;
+      display: block;
+      width: 260px;
+      height: 250px;
+      background-repeat: no-repeat;
+      background-position: center;
+      background-size: cover;
+      background-image: url(${loginBgImgTab});
+      // retina screen
+      @media (min-device-pixel-ratio: 2),
+        (min-resolution: 192dpi),
+        (min-resolution: 2dppx) {
+        background-image: url(${loginBgImgTabLarge});
+      }
+    }
+  }
+  @media screen and (min-width: 1280px) {
+    padding-top: 136px;
+    padding-bottom: 116px;
+    ::before {
+      top: 150px;
+      left: 76px;
+      width: 435px;
+      height: 420px;
+      background-image: url(${loginBgImgDesk});
+      // retina screen
+      @media (min-device-pixel-ratio: 2),
+        (min-resolution: 192dpi),
+        (min-resolution: 2dppx) {
+        background-image: url(${loginBgImgDeskLarge});
+      }
+    }
+    ::after {
+      content: '';
+      position: absolute;
+      top: 0;
+      right: 0;
+      z-index: -25;
+      display: block;
+      width: 57%;
+      height: 100vh;
+      background-repeat: no-repeat;
+      background-position: center;
+      background-size: cover;
+      background-image: linear-gradient(
+        to bottom,
+        rgba(255, 255, 255, 0.4),
+        rgba(255, 255, 255, 0.4)
+      );
+      backdrop-filter: blur(25px);
+    }
+  }
+`;
+
+export const CustomizedRegisterPage = styled(CustomizedLoginPage)`
+  @media screen and (min-width: 768px) {
+    padding-bottom: 48px;
+    ::before {
+      width: 274px;
+      background-image: url(${registerBgImgTab});
+      // retina screen
+      @media (min-device-pixel-ratio: 2),
+        (min-resolution: 192dpi),
+        (min-resolution: 2dppx) {
+        background-image: url(${registerBgImgTabLarge});
+      }
+    }
+  }
+  @media screen and (min-width: 1280px) {
+    padding-top: 52px;
+    padding-bottom: 52px;
+    ::before {
+      top: 153px;
+      left: 66px;
+      width: 452px;
+      height: 413px;
+      background-image: url(${registerBgImgDesk});
+      // retina screen
+      @media (min-device-pixel-ratio: 2),
+        (min-resolution: 192dpi),
+        (min-resolution: 2dppx) {
+        background-image: url(${registerBgImgDeskLarge});
+      }
+    }
+  }
+`;
+
+export const Title = styled.h1`
+  position: absolute;
+  z-index: -10;
+  font-weight: 400;
+  font-size: 30px;
+  line-height: calc(45 / 30);
+  color: ${p => p.theme.colors.black};
+  @media screen and (max-width: 1279px) {
+    top: 163px;
+    right: 153px;
+  }
+  @media screen and (min-width: 1280px) {
+    top: 598px;
+    left: 200px;
+  }
+`;
+
+export const HeaderLogo = styled.header`
+  a {
+    justify-content: center;
+  }
+`;
+
+export const Wrapper = styled.div`
+  @media screen and (min-width: 768px) {
+    width: 533px;
+    margin-left: auto;
+    margin-right: auto;
+    padding: 40px 58.5px 62px 65px;
+    border-radius: ${p => p.theme.borderRadius.r20};
+    background-color: ${p => p.theme.colors.white};
+  }
+  @media screen and (min-width: 1280px) {
+    margin-right: 91px;
+  }
+`;
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 40px;
+  margin-top: 60px;
+  margin-bottom: 20px;
+`;
+
+export const Label = styled.label`
+  position: relative;
+  svg {
+    position: absolute;
+    top: 8px;
+    left: 12.5px;
+    width: 24px;
+    height: 24px;
+    fill: ${p => {
+      if (p.value.length > 0) {
+        return !p.error ? p.theme.colors.btGreen : p.theme.colors.btPink;
+      }
+      return '#e0e0e0';
+    }};
+  }
+  div {
+    position: absolute;
+    top: 48px;
+    left: 0;
+    width: 100%;
+    height: 0;
+    border: ${p => p.theme.borderColor.c};
+    border-radius: 8px;
+  }
+  span {
+    position: absolute;
+    top: -4px;
+    left: -4px;
+    display: block;
+    width: ${p => (p.length < 13 ? (p.length / 12) * 102 : 102)}%;
+    height: 0;
+    border: 4px solid
+      ${p => (!p.error ? p.theme.colors.btGreen : p.theme.colors.btPink)};
+    border-radius: 8px;
+    box-shadow: ${p => p.theme.boxShadow.confirmPassword};
+  }
+  p {
+    position: absolute;
+    top: 4px;
+    left: 0;
+    margin: 0;
+    font-size: 10px;
+    line-height: calc(27 / 18);
+    color: ${p => p.theme.colors.btPink};
+  }
+`;
+
+export const Input = styled.input`
+  width: 100%;
+  padding: 8px 8px 8px 54.5px;
+  border: none;
+  border-bottom: ${p => p.theme.borderColor.comment};
+  font-size: 18px;
+  line-height: calc(27 / 18);
+  ::placeholder {
+    color: ${p => p.theme.colors.inputGray};
+  }
+`;
+
+export const FormButton = styled.button`
+  padding: 13px;
+  background: ${p => p.theme.colors.btGreen};
+  border: 1px solid transparent;
+  border-radius: ${p => p.theme.borderRadius.r20};
+  font-size: 18px;
+  font-weight: 400;
+  line-height: calc(27 / 18);
+  letter-spacing: 0.1em;
+  text-transform: uppercase;
+  color: ${p => p.theme.colors.white};
+  &:hover,
+  &:focus {
+    transform: scale(1.05);
+    box-shadow: 0px 6px 15px ${p => p.theme.colors.btGreen};
+  }
+  @media screen and (min-width: 768px) {
+    width: 300px;
+    margin-left: auto;
+    margin-right: auto;
+  }
+`;
+
+export const FormLink = styled(Link)`
+  display: block;
+  padding: 13px;
+  background-color: ${p => p.theme.colors.white};
+  border: ${p => p.theme.borderColor.b};
+  border-radius: ${p => p.theme.borderRadius.r20};
+  font-size: 18px;
+  line-height: calc(27 / 18);
+  text-align: center;
+  letter-spacing: 0.1em;
+  text-transform: uppercase;
+  text-decoration: none;
+  color: ${p => p.theme.colors.btBlue};
+  transition: all 0.3s;
+  &:hover,
+  &:focus {
+    transform: scale(1.05);
+    box-shadow: 0px 6px 15px ${p => p.theme.colors.btBlue};
+  }
+  @media screen and (min-width: 768px) {
+    width: 300px;
+    margin-left: auto;
+    margin-right: auto;
+  }
+`;
---
+++ src/redux/auth/authOperations.js
@@ -0,0 +1,97 @@
+import axios from 'axios';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import { toast } from 'react-toastify';
+
+
+export const baseAPI = axios.create({
+  baseURL: 'https://wallet.goit.ua/api',
+});
+
+const toastStyled = {
+  position: 'top-right',
+  autoClose: 5000,
+  hideProgressBar: false,
+  closeOnClick: true,
+  pauseOnHover: true,
+  draggable: true,
+  progress: undefined,
+  theme: 'colored',
+};
+
+const token = {
+  set(token) {
+    baseAPI.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+  },
+  unset() {
+    baseAPI.defaults.headers.common['Authorization'] = '';
+  },
+};
+
+export const register = createAsyncThunk(
+  'auth/register',
+  async (userData, { rejectWithValue }) => {
+    try {
+      const { data } = await baseAPI.post('/auth/sign-up', userData);
+      token.set(data.token);
+      return data;
+    } catch (error) {
+      if (error.response.status === 409) {
+        toast.error('User with such email already exists!', toastStyled);
+      } else {
+        toast.error('Validation error.', toastStyled);
+      }
+
+      return rejectWithValue(error.message);
+    }
+  }
+);
+
+export const login = createAsyncThunk(
+  'auth/login',
+  async (userData, { rejectWithValue }) => {
+    try {
+      const { data } = await baseAPI.post('/auth/sign-in', userData);
+      token.set(data.token);
+      return data;
+    } catch (error) {
+      if (error.response.status === 404) {
+        toast.error('User with such email not found!', toastStyled);
+      } else if (error.response.status === 403) {
+        toast.error('Provided password is incorrect!', toastStyled);
+      } else {
+        toast.error('Validation error.', toastStyled);
+      }
+
+      return rejectWithValue(error.message);
+    }
+  }
+);
+
+export const logout = createAsyncThunk(
+  'auth/logout',
+  async (_, { rejectWithValue }) => {
+    try {
+      await baseAPI.delete('/auth/sign-out');
+      token.unset();
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
+
+export const fetchCurrentUser = createAsyncThunk(
+  'auth/refresh',
+  async (_, { rejectWithValue, getState }) => {
+    const tokenCurrent = getState().auth.token;
+    if (!tokenCurrent) {
+      return rejectWithValue();
+    }
+    token.set(tokenCurrent);
+    try {
+      const { data } = await baseAPI.get('/users/current');
+      return data;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
