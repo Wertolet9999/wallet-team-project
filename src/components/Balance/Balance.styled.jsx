cac7cd7e25de90e7f82066304e4ec8444f776a51
Hermangsua <hermangsua@gmail.com>
2023-02-17 21:47:27 +0200

    balanceAdd

---
+++ src/components/Balance/Balance.styled.jsx
@@ -0,0 +1,41 @@
+import styled from 'styled-components';
+
+export const BalanceDiv = styled.div`
+  margin-top: 12px;
+  margin-bottom: 32px;
+  @media screen and (max-width: 767px) {
+    margin-right: auto;
+    margin-left: auto;
+  }
+  padding: 8px 32px 11px;
+  width: 100%;
+  background-color: white;
+  border-radius: 20px;
+  @media screen and (min-width: 376px) {
+    width: 336px;
+  }
+  @media screen and (min-width: 768px) {
+    margin-top: 28px;
+    margin-bottom: 0;
+    padding-left: 40px;
+    padding-right: 40px;
+  }
+  @media screen and (min-width: 1280px) {
+    width: 395px;
+  }
+`;
+
+export const TextBalance = styled.p`
+  font-family: Exo, sans-serif;
+  font-size: 12px;
+  line-height: 1.5;
+  text-transform: uppercase;
+  color: #a6a6a6;
+`;
+
+export const AmountBalance = styled.p`
+  margin-top: 12px;
+  color: #000000;
+  font-size: 30px;
+  line-height: calc(36 / 30);
+`;
