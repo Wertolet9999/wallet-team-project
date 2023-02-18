a9e7545d59311a721ac9ee10169e0c4205bfbf85
sydorenkorv <99117460+sydorenkorv@users.noreply.github.com>
2023-02-18 11:19:51 +0000

    logout

--- src/components/heder-nav/HeaderNav.jsx
+++ src/components/heder-nav/HeaderNav.jsx
@@ -2,17 +2,31 @@ import React from 'react';
 import { IoLogOutOutline } from 'react-icons/io5';
 import { useMedia } from 'react-use';
 import { List, LogOutButton, SpanDead } from './HeaderNav.styled';
+import { logout } from 'redux/auth/authOperations';
+import { useState } from 'react';
+import { LogoutForm } from 'components/ModalLogout/ModalLogout';
+import { useDispatch } from 'react-redux';
 
 const HeaderNav = () => {
   const isMobile = useMedia('(max-width: 767px)');
+  const dispatch = useDispatch();
+  const [isOpenLogoutModal, setIsOpenLogoutModal] = useState(false);
+
   return (
     <List>
       <li>
         <SpanDead>{/*{Name} {логика когбудет имя}*/} Name</SpanDead>
       </li>
       <li>
-        <LogOutButton>
-          <IoLogOutOutline /> {!isMobile && <SpanDead>Exit</SpanDead>}
+        {isOpenLogoutModal && (
+          <LogoutForm closeModalFunc={() => setIsOpenLogoutModal(false)} />
+        )}
+        <LogOutButton
+          onClick={() => {
+            isMobile ? dispatch(logout()) : setIsOpenLogoutModal(true);
+          }}
+        >
+          <IoLogOutOutline /> {!isMobile && 'Exit'}
         </LogOutButton>
       </li>
     </List>
