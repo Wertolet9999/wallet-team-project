c45e4579ef55fbace8944a55a7c9b49aed18358d
MSR333UA <matviivserhii@gmail.com>
2023-02-20 11:55:20 +0200

    fix

--- src/components/Header/Header.styled.js
+++ src/components/Header/Header.styled.js
@@ -1,20 +1,18 @@
 import styled from 'styled-components';
 
-
 export const HeaderTag = styled.header`
- background-color: white;
-    width: 100%;
-    z-index: 2;
-  }
+  background-color: white;
+  width: 100%;
+  z-index: 2;
 `;
 
 export const HeaderDiv = styled.div`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    width: 100%;
-    height: 60px;
-    @media (min-width: 768px) {
-        height: 80px;
-    }
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  height: 60px;
+  @media (min-width: 768px) {
+    height: 80px;
+  }
 `;
